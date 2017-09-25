0c2eb433233d0d47d8bca063759eb57cd62acf1a
Martin Bee <martbee88@gmail.com>
2017-09-25 09:41:21 -0500

    feat: add eslint, babel, setup nodemon

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": "airbnb",
+  "env": {
+    "node": true,
+  },
+  "rules": {
+    "spaced-comment": 0
+  },
+  "plugins": [
+    "import"
+  ],
+  "parser": "babel-eslint",
+};
